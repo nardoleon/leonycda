7000aa4fd900a5b824619f8533bdc3be4ef0424e
nardoleon <enndurak@gmail.com>
2017-01-20 12:46:56 +0100

    update personal webpage
    just few changements, still need a lot of work

--- personalwebsite/js/custom.js
+++ personalwebsite/js/custom.js
@@ -8,3 +8,57 @@ $('.readmore').click(function(){
     
 
 });
+
+var currentImage;
+
+$( ".portfolio" ).click(function(e) {
+
+ //console.info (e);
+ currentImage = e.target;
+ console.info (currentImage);
+
+ 
+  $(e.target)
+ 
+  .css('position','fixed')
+   .css('z-index','2')
+    .css('cursor','default')
+  
+  .animate({
+ 
+   width:"100%",
+   height:"100%",
+   margin:0,
+   top:0,
+   left:0,
+   
+    
+  }, 900, function() {
+
+   $(".close").show(900);
+
+  
+  })
+  
+  .clearQueue();
+console.log ( currentImage.queue);
+});
+
+$('.close').click(function() {
+$(".close").hide();
+$(currentImage)
+
+  .css('position','relative')
+   .css('z-index','1')
+     .css('cursor','pointer')
+   .animate({
+   width:"250px",
+   height:"250px",
+   margin:"10px"
+   }, 900, function() {
+			
+   
+
+  
+  });
+});
